c57ccecc524f71aaf715bf43e5e60ee742a73774
lindeaccount2 <2077915873@qq.com>
2022-11-22 20:36:17 +0800

    林正立,2022.11.22,20:36，捐款2.0

--- work/src/main/java/com/workdatabase/Controller/MoneyController.java
+++ work/src/main/java/com/workdatabase/Controller/MoneyController.java
@@ -4,6 +4,7 @@ package com.workdatabase.Controller;
 import com.workdatabase.domain.money;
 import com.workdatabase.mapper.MoneyMapper;
 import com.workdatabase.mapper.UserMapper;
+import com.workdatabase.resp.CommonResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,8 @@ public class MoneyController {
     private UserMapper userMapper;
 
     @GetMapping("/money")       //捐款操作
-    public String donation(@RequestParam Integer user_id, @RequestParam Integer push_Money, @RequestParam Integer id){
+    public CommonResp donation(@RequestParam Integer user_id, @RequestParam Integer push_Money, @RequestParam Integer id){
+        CommonResp<String> commonResp = new CommonResp();
         if(push_Money > 0){
             Integer current_Money = userMapper.SelectById_integral(user_id);
             if(push_Money<=current_Money){
@@ -34,29 +36,47 @@ public class MoneyController {
                     update_current += push_Money;
                     moneyMapper.Update(id,update_current);                //更新捐款数据
                     userMapper.Update_integral(user_id,current_Money);      ////更新用户拥有的积分
-                    return "Success!";
+                    commonResp.setContent("");
+                    commonResp.setSuccess(true);
+                    commonResp.setMessage("捐款成功");
+//                    return "Success!";
                 }
                 else {
                     update_current += need_Money;
                     moneyMapper.Update(id,update_current);                //更新捐款数据
                     current_Money += (push_Money - need_Money);
                     userMapper.Update_integral(user_id,current_Money);     //更新用户拥有的积分
-                    return "Success! 超额返回您：" + (push_Money - need_Money) + "元";
+                    commonResp.setContent("");
+                    commonResp.setSuccess(true);
+                    commonResp.setMessage("捐款成功,超额返回您：" + (push_Money - need_Money) + "元");
+//                    return "Success! 超额返回您：" + (push_Money - need_Money) + "元";
                 }
             }
             else {
-                return "Failed!";
+                commonResp.setContent("");
+                commonResp.setSuccess(false);
+                commonResp.setMessage("捐款失败");
+//                return "Failed!";
             }
         }
         else{
-            return "不能输入小于等于0的数!";
+            commonResp.setContent("");
+            commonResp.setSuccess(false);
+            commonResp.setMessage("不能输入小于等于0的数!");
+//            return "不能输入小于等于0的数!";
         }
+        return commonResp;
     }
 
     @GetMapping("/fresh")                   //2.用分页的方式，返回当前捐款信息
-    public List<money> fresh(@RequestParam Integer pageNum,@RequestParam Integer pageSize){
+    public CommonResp fresh(@RequestParam Integer pageNum, @RequestParam Integer pageSize){
+        CommonResp< List<money> > commonResp = new CommonResp();
         pageNum = (pageNum - 1)* pageSize;
-        return moneyMapper.Select_All(pageNum,pageSize);
+        commonResp.setContent(moneyMapper.Select_All(pageNum,pageSize));
+        commonResp.setSuccess(true);
+        commonResp.setMessage("请求数据成功");
+        return commonResp;
+//        return moneyMapper.Select_All(pageNum,pageSize);
     }
 
 
