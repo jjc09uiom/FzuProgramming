0eddb6745a7fb5ce6a6d1dbfb63ff4a01ec46ff5
lindeaccount2 <2077915873@qq.com>
2022-11-27 20:01:22 +0800

    林正立 11.27 20:00 实现后台web功能--“捐款对象”增删改

---
+++ demo/src/main/java/com/workdatabase/mapper/MybatisPlus/DonateMapperPlus.java
@@ -0,0 +1,7 @@
+package com.workdatabase.mapper.MybatisPlus;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.workdatabase.domain.donate;
+
+public interface DonateMapperPlus extends BaseMapper<donate> {
+}
